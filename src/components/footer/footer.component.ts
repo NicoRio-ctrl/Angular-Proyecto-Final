a40931aced646a091cdc6109659a1d76254be8c8
Nicolás Ezequiel Rio <nicorio2003@gmail.com>
2024-06-28 19:56:01 -0300

    ERROR DE ORTOGRAFÍA
    Pokémon es incontable, no lleva "s".

--- src/components/footer/footer.component.ts
+++ src/components/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <section>
-      <p>© 2024 - 2030 Mati / Nico</p><p>151 Pokémons (and some more)</p>
+      <p>© 2024 - 2030 Mati / Nico</p><p>151 Pokémon (and some more)</p>
     </section>
   `,
   styleUrl: './footer.component.css'
